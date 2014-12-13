9a6a39678616044507f288a87c7d974f6508e0af
jasonjacks <bunnyartistj@gmail.com>
2014-12-13 14:26:17 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,61 @@
+//INCLUDES
 #include <SDL.h>
 #include <stdio.h>
 #include <string>
 
-//function prototypes
-bool init(SDL_Window  *gWindow, SDL_Surface *gScreenSurface); //Starts up SDL and creates window
-bool loadMedia(SDL_Surface *gPaddle, SDL_Surface *gBlanker, SDL_Surface *gBall);
-int key_poll(bool &quit);
-void game_logic(SDL_Rect &dstrect, SDL_Rect &ballRect, int ball_vector_x, int ball_vector_y, int game_logix);
-void game_view(SDL_Window  *gWindow, SDL_Surface *gScreenSurface, SDL_Surface *gPaddle, SDL_Surface *gBlanker, SDL_Surface *gBall, SDL_Rect dstrect, SDL_Rect ballRect);
-void close(SDL_Window  *gWindow, SDL_Surface *gPaddle); //Frees media and shuts down SDL
+//FUNCTION PROTOTYPES
+bool init ( SDL_Window*  &gWindow,
+	     SDL_Surface* &gScreenSurface); //Starts up SDL and creates window
 
-//SDL resources
-SDL_Window* gWindow = NULL; //The window we'll be rendering to
+bool loadMedia ( SDL_Surface* &gPaddle, 
+	          SDL_Surface* &gBlanker, 
+		  SDL_Surface* &gBall ); //Loads media
+
+int key_poll ( bool &quit ); //Returns integer after polling user input
+
+void game_logic ( SDL_Rect &paddleRect, 
+	           SDL_Rect &ballRect, 
+		   int      &ball_vector_x, 
+		   int      &ball_vector_y, 
+		   int      game_logix); //Updates game state
+
+void game_view ( SDL_Window*  gWindow, 
+	          SDL_Surface* gScreenSurface, 
+		  SDL_Surface* gPaddle, 
+		  SDL_Surface* gBlanker, 
+		  SDL_Surface* gBall, 
+		  SDL_Rect     paddleRect, 
+		  SDL_Rect     ballRect); //Renders game state
+
+void close ( SDL_Window*  &gWindow, 
+	      SDL_Surface* &gPaddle); //Frees media and shuts down SDL
+
+//SDL RESOURCES
+ SDL_Window* gWindow        = NULL; //The window we'll be rendering to
 SDL_Surface* gScreenSurface = NULL; //The surface contained by the window
-SDL_Surface* gPaddle = NULL; //Paddle
-SDL_Surface* gBlanker = NULL; //Blanker
-SDL_Surface* gBall = NULL; //Ball (Square)
+SDL_Surface* gPaddle        = NULL; //Paddle
+SDL_Surface* gBlanker       = NULL; //Blanker
+SDL_Surface* gBall          = NULL; //Ball (Square)
 
+
+//MAIN
 int main( int argc, char* args[] )
 {
-	//paddle stationary = 2
-	int game_logix = 2;
+	//Game Variables
+	int game_logix = 2; //paddle stationary = 2
 
-	//paddle data - location
-	SDL_Rect dstrect;
-	dstrect.x = 0;
-	dstrect.y = 240;
-	dstrect.w = 15;
-	dstrect.h = 80;
+	SDL_Rect paddleRect; //paddle data - location
+	paddleRect.x = 0;
+	paddleRect.y = 240;
+	paddleRect.w = 15;
+	paddleRect.h = 80;
 		
-	//ball data - location and vector
-	SDL_Rect ballRect;
+	SDL_Rect ballRect; //ball data - location
 	ballRect.x = 50;
 	ballRect.y = 50;
 	ballRect.w = 5;
 	ballRect.h = 5;
-	int ball_vector_x = 3;
+	int ball_vector_x = 3; //ball data - vector
 	int ball_vector_y = 5;
 	
 	printf("starting...\n");
@@ -71,13 +91,13 @@ int main( int argc, char* args[] )
 				game_logix = key_poll(quit);
 
 				//Update game state - pass paddle location, ball location, ball vector components, paddle signal
-				game_logic(dstrect, ballRect, ball_vector_x, ball_vector_y, game_logix);
+				game_logic(paddleRect, ballRect, ball_vector_x, ball_vector_y, game_logix);
 
 				//Draw game world
-				game_view(gWindow,gScreenSurface,gPaddle, gBlanker, gBall, dstrect, ballRect);
+				game_view(gWindow, gScreenSurface, gPaddle, gBlanker, gBall, paddleRect, ballRect);
 
 				//Delay for keyboard signal stream
-				SDL_Delay( 50 );
+				SDL_Delay( 5 );
 			}
 		}
 	}

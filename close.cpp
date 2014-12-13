dc2067e7ebb39190136aaaaf42e518b79b2ea860
jasonjacks <bunnyartistj@gmail.com>
2014-12-13 14:28:36 -0500

    Update close.cpp

--- close.cpp
+++ close.cpp
@@ -1,7 +1,7 @@
 #include <SDL.h>
 #include <stdio.h>
 
-void close(SDL_Window  *gWindow, SDL_Surface *gPaddle)
+void close(SDL_Window* &gWindow, SDL_Surface* &gPaddle)
 {
 	printf("Closing...\n");
 	SDL_Delay(2000);

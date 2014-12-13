78e669b5d6178b49a71a4132d860ff7f42f3716b
jasonjacks <bunnyartistj@gmail.com>
2014-12-13 03:22:59 -0500

    Create close.cpp

---
+++ close.cpp
@@ -0,0 +1,22 @@
+#include <SDL.h>
+#include <stdio.h>
+
+void close(SDL_Window  *gWindow, SDL_Surface *gPaddle)
+{
+	printf("Closing...\n");
+	SDL_Delay(2000);
+
+	//Deallocate surface
+    SDL_FreeSurface( gPaddle );
+    gPaddle = NULL;
+
+	//Destroy window
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+
+	printf("Closed...\n");
+	SDL_Delay(2000);
+}
